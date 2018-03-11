c2475ea86c98bc8e83e4a71e9fa4cc65cd71af6a
Arseny Glembotsky <dartrabbit02@gmail.com>
2018-03-11 16:11:42 +0300

    Update Bot.java

--- src/main/java/fun/bot4/model/bot/Bot.java
+++ src/main/java/fun/bot4/model/bot/Bot.java
@@ -46,27 +46,4 @@ public class Bot extends AbstractNamedEntity {
 
   @OneToMany(fetch = FetchType.LAZY, mappedBy = "bot")
   private List<Quote> quotes;
-
-//  public static void main(String[] args) {
-//    ApiContextInitializer.init();
-//
-//    TelegramBotsApi botsApi = new TelegramBotsApi();
-//
-//    Bot telegramBot1 = new Bot();
-//    telegramBot1.setActive(true);
-//    telegramBot1.setBotToken("347037495:AAGtX8QqLKNIdi8hqPTDj40nCdHDNZ_Le7w");
-//    telegramBot1.setName("ShrekTheSecondBot");
-//
-//    Bot telegramBot2 = new Bot();
-//    telegramBot2.setActive(true);
-//    telegramBot2.setBotToken("569944003:AAFjdvqOX11T4TfB47cO4jU6Mr4My32uZos");
-//    telegramBot2.setName("Shrek");
-//
-//    try {
-//      botsApi.registerBot(telegramBot1);
-//      botsApi.registerBot(telegramBot2);
-//    } catch (TelegramApiException e) {
-//      e.printStackTrace();
-//    }
-//  }
 }
